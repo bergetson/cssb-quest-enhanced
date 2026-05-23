7d8de9f7a0bbf198160de69206e5732e6dbb905c
bergetson <79886426+bergetson@users.noreply.github.com>
2026-05-22 18:49:31 -0600

    Load mobile polish styles

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./mobile-polish.css";
 
 function mountApp() {
   const root = document.getElementById("root");
